450e27dd6f858951a6b25733cd2acf6fc8dae5f1
romina <romina.torres.b@gmail.com>
2017-05-21 12:16:28 -0400

    terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var fotos = document.getElementsByClassName('imagenes-varias')[0];
+
+pictures.forEach(function(f){
+	var imagen = document.createElement('img')
+	imagen.setAttribute("src",f);
+	imagen.setAttribute("class","los-pandas");
+	fotos.appendChild(imagen);	
+})
